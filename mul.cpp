cd9a4dbbe67e7d4203b7f198d512ce4446cee3c6
Pavlova Ruslana Eduarovna <lana.pavlova08@mail.ru>
2026-09-22 13:01:23 +0300

    изменен файл

--- mul.cpp
+++ mul.cpp
@@ -19,3 +19,24 @@ int main() {
             result = num1 - num2;
             cout << "Результат: " << result << endl;
             break;
+        case '':
+            result = num1 * num2;
+            cout << "Результат: " << result << endl;
+            break;
+        case '/':
+            if (num2 != 0) {
+                result = num1 / num2;
+                cout << "Результат: " << result << endl;
+            } else {
+                cout << "Ошибка: деление на ноль!" << endl;
+            }
+            break;
+        default:
+            cout << "Ошибка: неверный оператор!" << endl;
+    }
+
+    return 0;
+}
+
+
+
